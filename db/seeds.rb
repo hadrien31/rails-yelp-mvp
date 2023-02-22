50a04c6bfeef4fd5c6762ad45f553e048ee5a92d
hadrien mari <hadrienmari@yahoo.fr>
2023-02-22 15:47:24 +0100

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }
 #   Character.create(name: "Luke", movie: movies.first)
+ciel = {name: "le ciel", address: "21 rue haxo", phone_number: "0102030405", category: "french" }
+vigne = {name: "la vignet et l'olivier", address: "port de La Ciotat", phone_number: "0102030406", category: "italian" }
+le_camion_ugo = {name: "le camion d'ugo", address: "chemin sainte brigitte", phone_number: "0102030407", category: "italian" }
+quinta = {name: "la quinta", address: "grande route", phone_number: "0102030408", category: "french" }
+saigon = {name: "le bonheur de saigon", address: "la plage", phone_number: "0102030409", category: "japanese" }
+
+[ciel, vigne, le_camion_ugo, quinta, saigon].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
